dc079662091c89602fd906bb709a1abd201d9697
solcedro82 <solcedro82@gmail.com>
2024-06-24 07:41:14 -0300

    tabla funciones get post delete y put

--- js/tabla.js
+++ js/tabla.js
@@ -4,6 +4,8 @@ const url= 'https://66706ba60900b5f8724a981f.mockapi.io/taller/talleres';
 
 const tabla= document.querySelector("#tablaBody");
 
+const mensaje= document.querySelector("#div-mensaje");
+
 async function cargarTabla(){//get
     tabla.innerHTML= "";
     try {
@@ -11,18 +13,16 @@ async function cargarTabla(){//get
         let objeto= await respuesta.json();
         console.log(objeto);
         for(let talleres of objeto){
+            let codigo= talleres.id;
             let nombre= talleres.nombre;
             let mes= talleres.mes;
             let precio= talleres.precio;
             tabla.innerHTML += 
             `<tr> 
+                <td>${codigo}</td>
                 <td>${nombre}</td>
                 <td>${mes}</td>
                 <td>${precio}</td>
-                <td>
-                    <button class="borrar-fila" name="${talleres.id}">Borrar</button>
-                    <button class="editar-fila" name="${talleres.id}">Editar</button>
-                </td>
             </tr>`;
         }
     } catch (error) {
@@ -30,10 +30,19 @@ async function cargarTabla(){//get
     }
 }
 
+//<td>
+//<button class="borrar-fila" name="${talleres.id}">Borrar</button>
+//<button class="editar-fila" name="${talleres.id}">Editar</button>
+//</td>
+
+
 cargarTabla();
 
-let container= document.querySelector("#div-mensaje");
+
+
 async function agregarNuevaFila(){
+    //codigo es el id que me da mockapi solo para borrar utilizo mediante el input, pero cuando agregan...
+    let codigo= document.querySelector ("#codigo-taller").value;
     let nombre= document.querySelector("#nombre-taller").value;
     let mes= document.querySelector("#mes-taller").value;
     let precio= document.querySelector("#precio-taller").value
@@ -52,13 +61,61 @@ async function agregarNuevaFila(){
 
         if(respuesta.status == 201){
             console.log("Nueva fila agregada");
-            container.innerHTML= ("Se ha agregado un nuevo taller");
+            mensaje.innerHTML= ("Se ha agregado un nuevo taller");
         }
     } catch (error){
         console.log(error);
     }
 }
 
-async function borrarFila(){}
+async function borrarFila(){
+    mensaje.innerHTML= "";
+    let id= document.querySelector("#codigo-taller").value;
+    try{
+        let respuesta= await fetch (`${url}/${id}`,{
+            method:"DELETE"
+        });
+        if(respuesta.status == 200){
+            console.log("Fila Borrada");
+            mensaje.innerHTML= ("Se ha borrado un taller");
+            cargarTabla();
 
-async function editarFila(){}
+        }
+    
+    }  catch (error){
+        console.log(error);
+    }
+}
+
+async function editarFila(){
+    let codigo= document.querySelector ("#codigo-taller").value;
+    let nombre= document.querySelector("#nombre-taller").value;
+    let mes= document.querySelector("#mes-taller").value;
+    let precio= document.querySelector("#precio-taller").value
+    let nuevaFila={
+        "nombre": nombre,
+        "mes": mes,
+        "precio": precio,
+    }
+    try {
+        let respuesta= await fetch(`${url}/${codigo}`,{
+            "method": "PUT",
+            "headers": {"Content-type": "application/json"},
+            "body": JSON.stringify (talleres)
+        });
+
+        if(respuesta.status == 200){
+            console.log("Fila Editada");
+            mensaje.innerHTML= ("Se ha editado un taller");
+        }
+    }  catch (error){
+        console.log(error);
+    }
+}
+
+
+document.querySelector("#btn-agregarFila").addEventListener("click", agregarNuevaFila);
+document.querySelector("#btn-borrarFila").addEventListener("click", borrarFila);
+document.querySelector("#btn-editarFila").addEventListener("click", editarFila);
+
+
